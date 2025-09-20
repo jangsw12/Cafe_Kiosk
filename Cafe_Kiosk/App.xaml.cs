a84d76d969ee03772c54e7a1bd52472beccfc2c1
jangsw12 <156777305+jangsw12@users.noreply.github.com>
2025-09-20 22:07:58 +0900

    첫 커밋

---
+++ Cafe_Kiosk/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Cafe_Kiosk.ViewModels;
+using Cafe_Kiosk.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Cafe_Kiosk
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+
+            var mainView = Services.GetRequiredService<MainView>();
+            mainView.Show();
+        }
+
+        public new static App Current => (App)Application.Current;
+
+        public IServiceProvider Services { get; }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // ViewModels
+            services.AddSingleton<MainViewModel>();
+
+            // Views
+            services.AddSingleton(s => new MainView()
+            {
+                DataContext = s.GetRequiredService<MainViewModel>()
+            });
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ Cafe_Kiosk/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Cafe_Kiosk.ViewModels
+{
+    public class MainViewModel
+    {
+        public ICommand TestClickCommand { get; set; }
+
+        public MainViewModel()
+        {
+            TestClickCommand = new RelayCommand<object>(TestClick);
+        }
+
+        private void TestClick(object _)
+        {
+            MessageBox.Show("프로그램 시작");
+        }
+    }
+}
